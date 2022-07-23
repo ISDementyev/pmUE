4c2a4443368e98fd14c87ef044d16b8ea48daa21
Ilya S. Dementyev <65086972+ISDementyev@users.noreply.github.com>
2022-07-22 22:13:41 -0400

    Introduced Tools class and made all fxns static

--- Source/Tools.h
+++ Source/Tools.h
@@ -12,122 +12,126 @@
 #include <vector> // used for atom coordinates (c++ vector creation)
 #include <map> // used for creating dictionaries (maps) of atoms (keys) and their respective position vector (values)
 
-/**
-* Removes all hydrogens
-*/
-void AtomManager::RemoveHydrogens()
-{
-    // find all hydrogen atoms and collect them in an array
-    TArray<AActor*> AllHydrogens;
-    UGameplayStatics::GetAllActorsWithTag(GetWorld(), "AtomH", AllHydrogens);
 
-    if (AllHydrogens.Num() > 0) // only execute if there are hydrogens in the molecule
+class Tools {
+public:
+    /**
+     * Removes all hydrogens
+    */
+    static void AtomManager::RemoveHydrogens()
     {
-        // destroy (remove) all hydrogen atoms in the "game"
-        for (int i; i < AllHydrogens.Num(); i++)
+        // find all hydrogen atoms and collect them in an array
+        TArray<AActor*> AllHydrogens;
+        UGameplayStatics::GetAllActorsWithTag(GetWorld(), "AtomH", AllHydrogens);
+
+        if (AllHydrogens.Num() > 0) // only execute if there are hydrogens in the molecule
         {
-            AllHydrogens[i]->Destroy();
-        }
-    }
-    else
-    {
-        UE_LOG(LogTemp, Warning, TEXT("Called RemoveHydrogens() but no hydrogens are in the molecule"));
-    }
-}
-
-/**
- * Counts the number of atoms in the pdb file
- * @param FileName Name of the pdb file (string)
- * @param OnlyAtom Boolean that, if true, does not consider heteroatoms (HETATM rows in pdb file)
- * @return Integer - number of atoms in pdb file
-*/
-int NumberOfAtoms(std::string FileName, bool OnlyAtom = true)
-{
-    std::fstream MyFile;
-    int counter = 0;
-
-    MyFile.open(FileName, std::ios::in);
-
-    if (MyFile.is_open())
-    {
-        std::string line;
-        while (getline(MyFile, line))
-        {
-            std::string CheckAtom = line.substr(0, 6); // checks if the line starts with "ATOM  ", in conjuction with OnlyAtom bool
-
-            //std::cout << "CheckAtom: " << CheckAtom << "\n"; // for debugging
-
-            if (OnlyAtom && CheckAtom == "ATOM  ")
+            // destroy (remove) all hydrogen atoms in the "game"
+            for (int i = 0; i < AllHydrogens.Num(); i++)
             {
-                counter++;
-            }
-            else if (!OnlyAtom && (CheckAtom == "ATOM  " || CheckAtom == "HETATM"))
-            {
-                counter++;
+                AllHydrogens[i]->Destroy();
             }
         }
-        MyFile.close();
-    }
-    else
-    {
-        std::cout << "Cannot find/open file of name: " << FileName << std::endl;
-    }
-
-    return counter;
-}
-
-/**
- * Acquires coordinates of all atoms from a pdb file
- * @param FileName Name of the pdb input file (string)
- * @param OnlyAtom Boolean that, if true, does not consider heteroatoms (HETATM rows in pdb file)
- * @param Verbosity Tells the function to be verbose or not (true for verbosity)
- * @return Dictionary (map) containing atom names (key) and their coordinates (value)
-*/
-std::map<std::string, std::vector<double>> AcquireCoordinates(std::string FileName, bool OnlyAtom = true, bool Verbosity = false) // should return a map (dictionary) of the atoms and their corresponding decimal coordinates
-{
-    std::map<std::string, std::vector<double>> AtomCoordinates; // dictionary for atom coordinates
-    std::fstream MyFile;
-
-    MyFile.open(FileName, std::ios::in);
-
-    if (MyFile.is_open())
-    {
-        std::string line;
-        while (getline(MyFile, line)) // "scanner" loop
+        else
         {
-            std::string CheckAtom = line.substr(0, 4); // substr returns first 4 characters of the current line, we are looking for "ATOM", in conjuction with OnlyAtom bool
-            //std::cout << "Check Atom: " << CheckAtom << "\n"; //for debugging
-
-            std::vector<double> PositionVector; // (will be a) 1x3 vector containing the atom coordinates
-            std::string AtomName;
-
-            if (OnlyAtom && CheckAtom == "ATOM")
-            {
-                std::string AtomName = line.substr(13, 4);
-
-                double X = std::stod(line.substr(32, 6));
-                double Y = std::stod(line.substr(40, 6));
-                double Z = std::stod(line.substr(48, 6));
-
-                // std::cout << "X: " << x << "\n"; // for debugging
-
-                PositionVector.push_back(X);
-                PositionVector.push_back(Y);
-                PositionVector.push_back(Z);
-
-                //std::cout << "Atom Name: " << AtomName << "\n"; // for debugging
-            }
-
-            AtomCoordinates[AtomName] = PositionVector;
-
-            //std::cout << "Reading line: " << line << std::endl; // for debugging
+            UE_LOG(LogTemp, Warning, TEXT("Called RemoveHydrogens() but no hydrogens are in the molecule"));
         }
-        MyFile.close();
-    }
-    else
-    {
-        std::cout << "Cannot find/open file of name: " << FileName << std::endl;
     }
 
-    return AtomCoordinates;
-}
+    /**
+     * Counts the number of atoms in the pdb file
+     * @param FileName Name of the pdb file (string)
+     * @param OnlyAtom Boolean that, if true, does not consider heteroatoms (HETATM rows in pdb file)
+     * @return Integer - number of atoms in pdb file
+    */
+    static int NumberOfAtoms(std::string FileName, bool OnlyAtom = true)
+    {
+        std::fstream MyFile;
+        int counter = 0;
+
+        MyFile.open(FileName, std::ios::in);
+
+        if (MyFile.is_open())
+        {
+            std::string line;
+            while (getline(MyFile, line))
+            {
+                std::string CheckAtom = line.substr(0, 6); // checks if the line starts with "ATOM  ", in conjuction with OnlyAtom bool
+
+                //std::cout << "CheckAtom: " << CheckAtom << "\n"; // for debugging
+
+                if (OnlyAtom && CheckAtom == "ATOM  ")
+                {
+                    counter++;
+                }
+                else if (!OnlyAtom && (CheckAtom == "ATOM  " || CheckAtom == "HETATM"))
+                {
+                    counter++;
+                }
+            }
+            MyFile.close();
+        }
+        else
+        {
+            std::cout << "Cannot find/open file of name: " << FileName << std::endl;
+        }
+
+        return counter;
+    }
+
+    /**
+     * Acquires coordinates of all atoms from a pdb file
+     * @param FileName Name of the pdb input file (string)
+     * @param OnlyAtom Boolean that, if true, does not consider heteroatoms (HETATM rows in pdb file)
+     * @param Verbosity Tells the function to be verbose or not (true for verbosity)
+     * @return Dictionary (map) containing atom names (key) and their coordinates (value)
+    */
+    static std::map<std::string, std::vector<double>> AcquireCoordinates(std::string FileName, bool OnlyAtom = true, bool Verbosity = false) // should return a map (dictionary) of the atoms and their corresponding decimal coordinates
+    {
+        std::map<std::string, std::vector<double>> AtomCoordinates; // dictionary for atom coordinates
+        std::fstream MyFile;
+
+        MyFile.open(FileName, std::ios::in);
+
+        if (MyFile.is_open())
+        {
+            std::string line;
+            while (getline(MyFile, line)) // "scanner" loop
+            {
+                std::string CheckAtom = line.substr(0, 4); // substr returns first 4 characters of the current line, we are looking for "ATOM", in conjuction with OnlyAtom bool
+                //std::cout << "Check Atom: " << CheckAtom << "\n"; //for debugging
+
+                std::vector<double> PositionVector; // (will be a) 1x3 vector containing the atom coordinates
+                std::string AtomName;
+
+                if (OnlyAtom && CheckAtom == "ATOM")
+                {
+                    std::string AtomName = line.substr(13, 4);
+
+                    double X = std::stod(line.substr(32, 6));
+                    double Y = std::stod(line.substr(40, 6));
+                    double Z = std::stod(line.substr(48, 6));
+
+                    // std::cout << "X: " << x << "\n"; // for debugging
+
+                    PositionVector.push_back(X);
+                    PositionVector.push_back(Y);
+                    PositionVector.push_back(Z);
+
+                    //std::cout << "Atom Name: " << AtomName << "\n"; // for debugging
+                }
+
+                AtomCoordinates[AtomName] = PositionVector;
+
+                //std::cout << "Reading line: " << line << std::endl; // for debugging
+            }
+            MyFile.close();
+        }
+        else
+        {
+            std::cout << "Cannot find/open file of name: " << FileName << std::endl;
+        }
+
+        return AtomCoordinates;
+    }
+};
