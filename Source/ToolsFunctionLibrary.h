14d957330765cf48ec7487a389409168764d0f90
Ilya S. Dementyev <65086972+ISDementyev@users.noreply.github.com>
2022-08-12 22:46:17 -0400

    Added docstrings (ConvFileToString) + new function
    New function reads in file content as an FString - to be used for reading PDB files.

--- Source/ToolsFunctionLibrary.h
+++ Source/ToolsFunctionLibrary.h
@@ -14,14 +14,44 @@ class PMUEINTEGRATION_API UToolsFunctionLibrary : public UBlueprintFunctionLibra
 {
 	GENERATED_BODY()
 		UFUNCTION(BlueprintCallable)
-		static int32 NumberOfAtoms(FString Filename, bool OnlyAtom = true);
+		/**
+		 * Converts file content into string
+		 * @param Filename Name of the file
+		 * @return The file's content as an FString
+		*/
+		static FString ConvFileToString(FString Filename);
 
-		UFUNCTION(BlueprintCallable)	
-		static TSet<FString> GetUniqueElements(FString Filename);
 
 		UFUNCTION(BlueprintCallable)
-		FVector GetElementColourRGB(FString AtomSymbol);
+		/**
+		 * Counts the number of atoms in the pdb file
+		 * @param FileName Name of the pdb file (string)
+		 * @param bOnlyAtom Boolean that, if true, does not consider heteroatoms (HETATM rows in pdb file)
+		 * @return An integer number of atoms in pdb file
+		*/
+		static int32 NumberOfAtoms(FString Filename, bool bOnlyAtom);
+	
+		UFUNCTION(BlueprintCallable)
+		/**
+		 * Returns set of all unique elements found in the pdb - useful for blueprint generation/modification
+		 * @param FileName The pdb file's name
+		 * @return Set of all unique elements (not including hetatms)
+		*/
+		static TSet<FString> UniqueElements(FString Filename);
 
 		UFUNCTION(BlueprintCallable)
-		double GetVDWRadius(FString AtomSymbol);
+		/**
+		 * Getter function for a 3-vector corresponding to the input atom's color
+		 * @param ChemicalSymbol The atom's chemical symbol, as a string
+		 * @return RGB vector corresponding to the input atom
+		*/
+		static FVector GetElementColourRGB(FString ChemicalSymbol);
+		
+		UFUNCTION(BlueprintCallable)
+		/**
+		 * Returns van der Waals radius of input atom, based on the averages of Tables 1, 2 and 7 in "Van der Waals Radii of Elements" (Batsanov, 2001).
+		 * @param ChemicalSymbol The atom's chemical symbol
+		 * @return The van der Waals radius of the atom (in nm)
+		*/
+		static float GetVDWRadius(FString ChemicalSymbol);
 };
