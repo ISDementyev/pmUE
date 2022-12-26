be6afc5c88e4597908fa2997c1b5c6d36a53e578
Ilya S. Dementyev <65086972+ISDementyev@users.noreply.github.com>
2022-12-26 17:50:02 -0500

    Finished carbon spawner implementation file

--- Source/CarbonSpawner.cpp
+++ Source/CarbonSpawner.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "CarbonSpawner.h"
 #include "ToolsFunctionLibrary.h"
 
@@ -9,6 +8,22 @@ ACarbonSpawner::ACarbonSpawner()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+	InstancedStaticMeshComponentCarbon = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Instanced Static Mesh"));
+
+	//RootComponent = InstancedStaticMeshComponentCarbon;
+
+	InstancedStaticMeshComponentCarbon->SetMobility(EComponentMobility::Movable);
+	InstancedStaticMeshComponentCarbon->SetCollisionProfileName(UCollisionProfile::NoCollision_ProfileName);
+	InstancedStaticMeshComponentCarbon->SetGenerateOverlapEvents(false);
+}
+
+/**
+ * Constructs the atom instances in CarbonSpawner blueprint
+ * @param Transform The locations (position vectors) at which to spawn the instances (atoms)
+*/
+void ACarbonSpawner::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
 
 }
 
@@ -17,15 +32,20 @@ void ACarbonSpawner::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// "global" variables - should be kept in a separate file called by all spawner Actors
+	// file-specific variables - should be kept in a separate file called by all atom spawner Actors
 	FString PDBContent = UToolsFunctionLibrary::ConvFileToString("Methane.pdb");
 	FVector CentroidCoordinate = UToolsFunctionLibrary::Centroid(PDBContent, false);
-	TMap<int32, FString> IndicesAndAtomNames = UToolsFunctionLibrary::AtomNameAndIndex(PDBContent, false);
 
 	// variables specific to this spawner
 	FString CurrentElement{ "C " };
 	int32 NumberOfAtoms = UToolsFunctionLibrary::NumberOfAtomsSingleElement(PDBContent, CurrentElement, false);
-	Transforms.Empty(NumberOfAtoms);
+	TransformsCarbon.Empty(NumberOfAtoms);
+
+	// generates data array of carbons to be spawned
+	UToolsFunctionLibrary::CCESAtomGeneration(PDBContent, TransformsCarbon, CentroidCoordinate, CurrentElement, false);
+
+	// spawns the actual static mesh	
+	InstancedStaticMeshComponentCarbon->AddInstances(TransformsCarbon, true);
 
 }
 
