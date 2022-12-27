f98b74c899c1e579b17d9bc93a77587b22be351f
Ilya S. Dementyev <65086972+ISDementyev@users.noreply.github.com>
2022-12-26 22:16:41 -0500

    Added nitrogen and oxygen spawners
    Functions will have to be edited to take the element symbol from the last columns, rather than from the atom name.

---
+++ Source/NitrogenSpawner.cpp
@@ -0,0 +1,57 @@
+#include "NitrogenSpawner.h"
+#include "ToolsFunctionLibrary.h"
+#include "FileData.h"
+
+// Sets default values
+ANitrogenSpawner::ANitrogenSpawner()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	InstancedStaticMeshComponentNitrogen = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Instanced Static Mesh"));
+
+	//RootComponent = InstancedStaticMeshComponentNitrogen;
+
+	InstancedStaticMeshComponentNitrogen->SetMobility(EComponentMobility::Movable);
+	InstancedStaticMeshComponentNitrogen->SetCollisionProfileName(UCollisionProfile::NoCollision_ProfileName);
+	InstancedStaticMeshComponentNitrogen->SetGenerateOverlapEvents(false);
+}
+
+/**
+ * Constructs the atom instances in NitrogenSpawner blueprint
+ * @param Transform The locations (position vectors) at which to spawn the instances (atoms)
+*/
+void ANitrogenSpawner::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+}
+
+// Called when the game starts or when spawned
+void ANitrogenSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// file-specific variables
+	FileData FD;
+	FString PDBContent = FD.PDBContent;
+	FVector CentroidCoordinate = FD.CentroidCoordinate;
+
+	// variables specific to this spawner
+	FString CurrentElement{ "N " };
+	int32 NumberOfAtoms = UToolsFunctionLibrary::NumberOfAtomsSingleElement(PDBContent, CurrentElement, false);
+	TransformsNitrogen.Empty(NumberOfAtoms);
+
+	// generates data array of atoms to be spawned
+	UToolsFunctionLibrary::CCESAtomGeneration(PDBContent, TransformsNitrogen, CentroidCoordinate, CurrentElement, false);
+
+	// spawns the actual static mesh
+	InstancedStaticMeshComponentNitrogen->AddInstances(TransformsNitrogen, true);
+}
+
+// Called every frame
+void ANitrogenSpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
