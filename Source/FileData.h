3d14ab79f419e430945535cff1ec70a05aee7f35
Ilya S. Dementyev <65086972+ISDementyev@users.noreply.github.com>
2023-04-23 19:57:44 -0400

    Added new relevant file data

--- Source/FileData.h
+++ Source/FileData.h
@@ -12,8 +12,11 @@ public:
 	FileData();
 	~FileData();
 
-	FString FileName{ "Methane.pdb" };
+	bool AtomOnly{ false };
+	FString FileName{ "AceticAcid.pdb" };
 	FString PDBContent = UToolsFunctionLibrary::ConvFileToString(FileName);
-	FVector CentroidCoordinate = UToolsFunctionLibrary::Centroid(PDBContent, false);
+	FVector CentroidCoordinate = UToolsFunctionLibrary::Centroid(PDBContent, AtomOnly);
+	int32 NAtoms = UToolsFunctionLibrary::NumberOfAtoms(PDBContent, AtomOnly);
+	TMap<int32, FString> IndexAndCoord = UToolsFunctionLibrary::AtomIndexAndCoordMap(PDBContent, NAtoms, AtomOnly);
 
 };
